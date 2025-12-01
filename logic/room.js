0a5633c2cb086c8d405f80de2d2e73cc96ce3704
Drewcfre <ajaarsma2@gmail.com>
2025-12-01 12:36:59 -0800

    Added outline for game logic.

---
+++ logic/room.js
@@ -0,0 +1,12 @@
+export class Room {
+    constructor(backdropURL, roomNPCs, dialogList, dialogResponses, correctAnswer, unlocked) {
+        this.backdropURL = backdropURL;
+        this.roomNPCs = roomNPCs;
+        this.dialogList = dialogList;
+        this.dialogResponses = dialogResponses;
+        this.correctAnswer = correctAnswer;
+        this.unlocked = unlocked;
+    }
+
+
+}
--- main.js
+++ main.js
@@ -1,5 +1,18 @@
+import Room from "logic/room";
+
 const myCanvas = document.getElementById("gameScene");
 const ctx = myCanvas.getContext("2d");
 
 ctx.fillStyle="blue";
 ctx.fillRect(0, 0, 150, 50);
+
+let appendURL = "resources/images/"
+
+let roomList = [
+    new Room(`${appendURL}commons.png`, "MASCOT", ["Welcome to the Commons!", "How do you like it?"], ["I love it!", "It's okay.", "Not a big fan.", "I'd rather die than stay here"], 1, true),
+    new Room(`${appendURL}coachingRoom.png`, `COACH`, ["Hey, welcome to the coaching room!", "Are you new here?"], ["Yeah, I'm a neubie.", "I think so.", "I'm a first-year", "I've been here three years. How do you not know me?"], 1, true),
+];
+
+let currentRoom = roomList[0];
+
+function setRoom() {}
