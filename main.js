f89ea00bb4c0e9ed92a41728781437193fef6425
Zultrafury <zultrafury@gmail.com>
2025-12-09 19:22:08 -0700

    basic game loop stuff
    + Dynamically created button onclick events
    + Basic game logic for progressing through the map
    + Canvas updates with some basic graphics based on room

--- main.js
+++ main.js
@@ -2,25 +2,52 @@ import Room from "./logic/room.js";
 
 const myCanvas = document.getElementById("gameScene");
 const ui = document.getElementById("gameUI");
+const buttons = document.getElementById("buttonContainer");
 const ctx = myCanvas.getContext("2d");
 
-ctx.fillStyle="blue";
 ctx.fillRect(0, 0, 150, 50);
+ctx.fillStyle="blue";
 
 let appendURL = "resources/images/";
 
 let roomList = [
     new Room(`${appendURL}commons.png`, "MASCOT", ["Welcome to the Commons!", "How do you like it?"], ["I love it!", "It's okay.", "Not a big fan.", "I'd rather die than stay here"], 1, true),
     new Room(`${appendURL}coachingRoom.png`, `COACH`, ["Hey, welcome to the coaching room!", "Are you new here?"], ["Yeah, I'm a neubie.", "I think so.", "I'm a first-year", "I've been here three years. How do you not know me?"], 1, true),
+    new Room(`image`, "npcs", ["Dialog"], ["1", "2", "3", "4"], 1, true),
+    new Room(`image`, "npcs", ["Dialog"], ["1", "2", "3", "4"], 1, true),
+    new Room(`image`, "npcs", ["Dialog"], ["1", "2", "3", "4"], 1, true),
+    new Room(`image`, "npcs", ["Dialog"], ["1", "2", "3", "4"], 1, true),
 ];
 
 let currentRoom = roomList[0];
+let roomIndex = 0;
 
 setRoom();
 
+for (let index = 0; index < buttons.children.length; index++) {
+    const element = buttons.children[index];
+    element.onclick = () => {
+        let cap = element.id.charAt(0).toUpperCase() + 
+            element.id.slice(1);
+        
+        if (index == currentRoom.correctAnswer) roomIndex++;
+        else alert(cap+" button clicked!");
+    }
+}
+
 function setRoom() {
+    currentRoom = roomList[roomIndex];
+
     ui.children[0].innerHTML = "";
     currentRoom.dialogList.forEach(element => {
         ui.children[0].innerHTML += element+"<br>";
     });
+
+    for (let index = 0; index < buttons.children.length; index++) {
+        const element = buttons.children[index];
+        element.textContent = currentRoom.dialogResponses[index];
+    }
+
+    ctx.clearRect(0,0,myCanvas.width,myCanvas.height)
+    ctx.fillRect((roomIndex%2)*150,Math.floor(roomIndex/2)*50, 150, 50);
 }
