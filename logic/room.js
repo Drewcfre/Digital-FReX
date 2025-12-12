b4b0440d4edbea47c0b059ea93e8524d1449a024
Drewcfre <ajaarsma2@gmail.com>
2025-12-11 23:13:51 -0800

    Tweaked some code for readability; converted files to pngs; added some images of the rooms.

--- logic/room.js
+++ logic/room.js
@@ -1,4 +1,4 @@
-export default class Room {
+class Room {
     constructor(backdropURL, roomNPCs, dialogList, dialogResponses, correctAnswer, incorrectResponse,unlocked) {
         this.backdropURL = backdropURL;
         this.roomNPCs = roomNPCs;
@@ -9,3 +9,79 @@ export default class Room {
         this.unlocked = unlocked;
     }
 }
+
+const filePath = "resources/images";
+export const roomList = [
+    new Room(
+        `${filePath}/locations/commons.png`,
+        `${filePath}/characters/encouragingLlamaGuide.png`,
+        ["Commons:", "Answer questions correctly and you'll be able to progress through the school.", "Reach the top to win!"],
+        ["Let's go!", "Wait, what?", "Don't feel like it.", "I'll just stay here, thanks."],
+        0,
+        "Are you sure? Cmon! Lets play!",
+        true
+    ),
+    new Room(
+        `${filePath}/locations/coachingRoom.png`,
+        `${filePath}/characters/talkingLlamaCoach.png`,
+        ["Coaching Room:", "Hey, do you know which room is only accessible through the coaching room?", "It seems important..."],
+        ["Cafeteria", "Ordinary Classroom", "Testing Room", "Server Room"],
+        2,
+        "No, that doesn't sound right, there's gotta be a different room connected.",
+        true
+    ),
+    new Room(
+        `${filePath}/locations/llamaBase.png`,
+        `${filePath}/characters/Burk_BSIS.png`,
+        ["Llama Base:", "Neumont's got a pretty cool mascot, eh?", "Would you happen to know what llama is in hexadecimal?"],
+        ["6A6A616D61", "5050515D51", "Hex-what now?", "6C6C616D61"],
+        3,
+        "Nope! This is a tricky one!",
+        true
+    ),
+    new Room(
+        `${filePath}/locations/classroom.png`,
+        `${filePath}/characters/cantera_BSSE.png`,
+        ["Classroom:", "Welcome to the Classroom! You should try a challenge", "This wont be the same as the questions before, you'll need to try a little harder!"],
+        ["I guess I can try!", "No I'm scared!", "I don't know...", "That seems too hard"],
+        0,
+        "Sorry, you gotta try it!",
+        false
+    ),
+    new Room(
+        `${filePath}/locations/llamaLounge.png`,
+        `${filePath}/characters/maple_BSAAI_BSGD.png`,
+        ["Llama Lounge:", "The lounge is a great place to focus and unwind!", "Speaking of focus, how long are quarters at Neumont?"],
+        ["8 weeks", "10 weeks", "12 weeks", "15 weeks"],
+        1,
+        "That answer seems off..." ,
+        true
+    ),
+    new Room(
+        `${filePath}/locations/registrar.png`,
+        `${filePath}/characters/pritchard_MSAAI.png`,
+        ["Registrar:", "Neumont has tons of classes available to choose from!", "Which of the following is NOT a degree program at Neumont?"],
+        ["BS Computer Science", "BS Software Engines", "MS Artificial Intelligence", "BS Software & Game Dev"],
+        1,
+        "I think we teach that here...",
+        true
+    ),
+    new Room(
+        `${filePath}/locations/store.png`,
+        `${filePath}/characters/krebs_BSCS_BSTM.png`,
+        ["Student Store:", "Commercialism? Count me in!", "How much is the average starting salary for Computer Science graduates in the US?"],
+        ["$55k", "$65k", "80k", ">$100k"],
+        3,
+        "I think it might be more than that...",
+        true
+    ),
+    new Room(
+        `${filePath}/locations/office.png`,
+        `${filePath}/characters/neumontPresident.png`,
+        ["President's Office", "Welcome to the President's Office! Can you beat the final challenge and become Neubie Supreme?", "This will be your hardest challenge yet, do you think you can do it?"],
+        ["I know I can!", "No way!", "This is way too difficult!", "...I cant do it..."],
+        0,
+        "Sorry, you gotta try it!",
+        false
+    ),
+];
--- main.js
+++ main.js
@@ -1,73 +1,10 @@
-import Room from "./logic/room.js";
+import {roomList} from "./logic/room.js";
 
 const myCanvas = document.getElementById("gameScene");
 const ui = document.getElementById("gameUI");
 const buttons = document.getElementById("buttonContainer");
 const ctx = myCanvas.getContext("2d");
 
-ctx.fillRect(0, 0, 150, 50);
-ctx.fillStyle="blue";
-
-let appendURL = "resources/images/";
-
-let roomList = [
-    new Room(`${appendURL}commons.png`, "MASCOT", 
-        ["Commons:",
-            "Answer questions correctly and you'll be able to progress through the school.",
-            "Reach the top to win!"], 
-        ["Let's go!","Wait, what?","Don't feel like it.","I'll just stay here, thanks."],
-        0, 'Are you sure? Cmon! Lets play',true),
-    new Room(`${appendURL}coachingRoom.png`, `COACH`, 
-        ["Coaching Room:",
-            "Hey, do you know which room is only accessible through the coaching room?",
-            "It seems important..."], 
-        ["Cafeteria", "Ordinary Classroom", "Testing Room", "Server Room"],
-        2, "No, that doesn't sound right, there's gotta be a different room connected",true),
-    new Room(`image`, "npcs",
-        ["Llama Base:",
-            "Neumont's got a pretty cool mascot, eh?",
-            "Would you happen to know what llama is in hexidecimal?"
-        ],
-        ["6A6A616D61", "5050515D51", "Hexi-what now?", "6C6C616D61"],
-        3, "Nope! This is a tricky one!", true),
-    new Room(`image`,`npcs`,
-        ["Classroom",
-            "Welcome to the Classroom! You should try a challenge",
-            "This wont be the same as the questions before, you'll need to try a little harder!"
-        ],
-        ["I guess I can try!","No I'm scared!","I don't know...","That seems too hard"],
-        0, "Sorry, you gotta try it!"
-    ),
-    new Room(`image`, "npcs",
-        ["Llama Lounge:",
-            "The lounge is a great place to focus and unwind!",
-            "Speaking of focus, how long are quarters at Neumont?"
-        ],
-        ["8 weeks", "10 weeks", "12 weeks", "15 weeks"],
-        1,"That answer seems off" ,true),
-    new Room(`image`, "npcs",
-        ["Registrar:",
-            "Neumont has tons of classes available to choose from!",
-            "Which of the following is NOT a degree program at Neumont?"
-        ],
-        ["BS Computer Science", "BS Software Engines", "MS Artificial Intelligence", "BS Software & Game Dev"],
-        1, "I don't think we teach that here", true),
-    new Room(`image`, "npcs",
-        ["Student Store:",
-            "Commercialism? Count me in!",
-            "How much is the average starting salary for Computer Science graduates in the US?"
-        ],
-        ["$55k", "$65k", "80k", ">$100k"],
-        3,"I think it might be more than that" ,true),
-    new Room(`image`,`npcs`,
-        ["President's Office",
-            "Welcome to the President's Office! Can you beat the final challenge and become Neubie Supreme?",
-            "This will be your hardest challenge yet, do you think you can do it?"
-        ],
-        ["I know I can!","No way!","This is way too difficult!","...I cant do it..."],
-        0, "Sorry, you gotta try it!"
-    ),
-];
 
 let currentRoom = roomList[0];
 let roomIndex = 0;
@@ -76,34 +13,34 @@ setRoom();
 normalRoom();
 
 function normalRoom(){
-for (let index = 0; index < buttons.children.length; index++) {
-    const element = buttons.children[index];
-    element.onclick = () => {
-        let cap = element.id.charAt(0).toUpperCase() + 
-            element.id.slice(1);
-        
-        if (index == currentRoom.correctAnswer) {
-            if(roomIndex == 3){
-                puzzleOne();
-            }else if(roomIndex == roomList.length - 1){
-                finalPuzzle();
-            }else if (roomIndex >= roomList.length - 1) alert("Congrats, you won!")
-            else {
-                roomIndex++;
-                setRoom();
+    for (let index = 0; index < buttons.children.length; index++) {
+        const element = buttons.children[index];
+        element.onclick = () => {
+            let cap = element.id.charAt(0).toUpperCase() +
+                element.id.slice(1);
+
+            if (index === currentRoom.correctAnswer) {
+                if(roomIndex === 3){
+                    puzzleOne();
+                }else if(roomIndex === roomList.length - 1){
+                    finalPuzzle();
+                }else if (roomIndex >= roomList.length - 1) alert("Congrats, you won!")
+                else {
+                    roomIndex++;
+                    setRoom();
+                }
+
             }
-            
+            else alert(""+currentRoom.incorrectResponse+"\nTry again!");
         }
-        else alert(""+currentRoom.incorrectResponse+"\nTry again!");
     }
-}
 }
 
 function puzzleOne() {
     let puzzleOptions = ["say('${i}: this number is even",'if(i%2 == 0)','i++','Submit']
     currentRoom = roomList[roomIndex]
     ui.children[0].innerHTML = "";
-    let challengeDialog = ['Welcome to the challenge! Fill in the psuedocode in the right order to make a for loop that shows even numbers',
+    let challengeDialog = ['Welcome to the challenge! Fill in the pseudocode in the right order to make a for loop that shows even numbers',
         'for(i){','a{','b','}','c','}'
     ]
     let selectionOrder = []
@@ -117,47 +54,47 @@ function puzzleOne() {
         element.textContent = puzzleOptions[index];
     }
 
-    ctx.clearRect(0,0,myCanvas.width,myCanvas.height)
-    ctx.fillRect((roomIndex%2)*150,112.5-(Math.floor(roomIndex/2)*37.5), 150, 37.5);
+    //ctx.clearRect(0,0,myCanvas.width,myCanvas.height)
+    //ctx.fillRect((roomIndex%2)*150,112.5-(Math.floor(roomIndex/2)*37.5), 150, 37.5);
 
     for (let index = 0; index < buttons.children.length; index++) {
-    const element = buttons.children[index];
-    element.onclick = () => {
-        if (index == 3) {
-           if(areArraysIdentical(correctSelection,selectionOrder)){
-            alert('Correct! You completed the puzzle!')
-            roomIndex++;
-            setRoom();
-            normalRoom();
-           }else{
-            alert('Incorrect! Try again!')
-            selectionOrder = [];
-           }
-        }else{
-             if(selectionOrder.includes(index)){
-                alert('You already added this element');
+        const element = buttons.children[index];
+        element.onclick = () => {
+            if (index === 3) {
+                if(areArraysIdentical(correctSelection,selectionOrder)){
+                    alert('Correct! You completed the puzzle!')
+                    roomIndex++;
+                    setRoom();
+                    normalRoom();
+                }else{
+                    alert('Incorrect! Try again!')
+                    selectionOrder = [];
+                }
             }else{
-                selectionOrder.push(index);
+                if(selectionOrder.includes(index)){
+                    alert('You already added this element');
+                }else{
+                    selectionOrder.push(index);
+                }
             }
         }
     }
 }
-}
 
 function areArraysIdentical(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
+    if (arr1.length !== arr2.length) {
+        return false;
     }
-  }
-  return true;
+    for (let i = 0; i < arr1.length; i++) {
+        if (arr1[i] !== arr2[i]) {
+            return false;
+        }
+    }
+    return true;
 }
 
 function finalPuzzle(){
-   let puzzleOptions = ["count++",'return count','list[i] == target','Submit']
+    let puzzleOptions = ["count++",'return count','list[i] == target','Submit']
     currentRoom = roomList[roomIndex]
     ui.children[0].innerHTML = "";
     let challengeDialog = ['Welcome to the final challenge! Fill in the pseudocode in the right order to make a function that counts how many times a target appears in a list.',
@@ -174,28 +111,28 @@ function finalPuzzle(){
         element.textContent = puzzleOptions[index];
     }
 
-    ctx.clearRect(0,0,myCanvas.width,myCanvas.height)
-    ctx.fillRect((roomIndex%2)*150,112.5-(Math.floor(roomIndex/2)*37.5), 150, 37.5);
+    //ctx.clearRect(0,0,myCanvas.width,myCanvas.height)
+    //ctx.fillRect((roomIndex%2)*150,112.5-(Math.floor(roomIndex/2)*37.5), 150, 37.5);
 
     for (let index = 0; index < buttons.children.length; index++) {
-    const element = buttons.children[index];
-    element.onclick = () => {
-        if (index == 3) {
-           if(areArraysIdentical(correctSelection,selectionOrder)){
-            alert('Correct! You completed the puzzle!\nCongratulations! You beat Digital FReX!')
-           }else{
-            alert('Incorrect! Try again!')
-            selectionOrder = [];
-           }
-        }else{
-             if(selectionOrder.includes(index)){
-                alert('You already added this element');
+        const element = buttons.children[index];
+        element.onclick = () => {
+            if (index === 3) {
+                if(areArraysIdentical(correctSelection,selectionOrder)){
+                    alert('Correct! You completed the puzzle!\nCongratulations! You beat Digital FReX!')
+                }else{
+                    alert('Incorrect! Try again!')
+                    selectionOrder = [];
+                }
             }else{
-                selectionOrder.push(index);
+                if(selectionOrder.includes(index)){
+                    alert('You already added this element');
+                }else{
+                    selectionOrder.push(index);
+                }
             }
         }
     }
-}
 }
 
 function setRoom() {
@@ -213,4 +150,12 @@ function setRoom() {
 
     ctx.clearRect(0,0,myCanvas.width,myCanvas.height)
     ctx.fillRect((roomIndex%2)*150,112.5-(Math.floor(roomIndex/2)*37.5), 150, 37.5);
+
+    let backgroundImg = new Image();
+    backgroundImg.src = currentRoom.backdropURL;
+    backgroundImg.onload = () => ctx.drawImage(backgroundImg, 0, 0);
+
+    let npcImg = new Image();
+    npcImg.src = currentRoom.roomNPCs;
+    npcImg.onload = () => ctx.drawImage(npcImg, 15, 175, 300, 400);
 }
